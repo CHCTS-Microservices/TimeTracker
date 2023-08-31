d4a96bc9098ecaa73d7eb9287612e602c32b9d2a
Artic13 <akshayj13@hotmail.com>
2023-08-31 20:21:07 +1000

    initialised Cosmos file.

---
+++ src/app/utils/cosmos.tsx
@@ -0,0 +1,27 @@
+import { CosmosClient, JSONObject } from "@azure/cosmos";
+
+class API{
+
+    // Read a data
+    read(){
+        console.log('Hello world1');
+    }
+
+    // create data
+    create(){
+        console.log('create');
+    }
+
+    // delete data
+    delete(){
+        console.log('delete');
+    }
+
+    // update data
+    update(){
+        console.log('update');
+    }
+
+
+}
+export default API;
