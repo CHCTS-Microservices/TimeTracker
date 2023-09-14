a5b3ed1c42435a781f073448095a1afb295c43f6
Zylan <126135306+Zippland@users.noreply.github.com>
2023-09-14 22:31:43 +1000

    Update page.tsx

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,14 +1,68 @@
-// Main Page
+export default function Page() {
+        
+    return (
+        <>
+            <div className="p-4 flex">
+                {/* Left 1/3 */}
+                <div className="w-1/3">
+                    {/* Button */}
+                    <button 
+                        className="w-80 h-20 mt-4 ml-8 bg-blue-500 text-2xl flex-grow text-white rounded"
+                        
+                        style={{ 
+                            width: '330px', 
+                            height: '75px', 
+                            animationDuration: '0ms'
+                        }}
+                    >
+                        Create Activity
+                    </button>
+                    {/* Sidebar */}
+                    <div className="text-4xl text-white">
+                        Tao's work here
+                    </div>
+                </div>
+    
+                {/* Right 2/3 */}
+                <div className="w-2/3 ml-10">
+                    {/* Form - Tracking */}
+                    <div 
+                        className="gap-2.5 flex flex-col justify-between font-bold p-4 rounded-lg"
+                        style={{ 
+                            width: '1020px', 
+                            height: '889px',
+                            backgroundColor: '#fbd48c',
+                            fontFamily: 'Arial'
+                        }}
+                    >
+                        {/* First Element - 25% */}
+                        <div className="flex justify-between items-center rounded-lg p-4" style={{ height: '25%'}}>
+                            {/* First Sub-Element */}
+                            <div className="bg-244982 text-4xl text-black" >
+                                Joseph's work here
+                            </div>
+                            {/* Second Sub-Element */}
+                            <div className="flex justify-between items-center" style={{width: '40%'}}>
+                            <div className="bg-white text-black p-4 text-6xl flex-grow rounded-lg" id="timer">00:00:00</div>
+                            <button className="bg-2e9427 text-white p-4 rounded-lg" id="tracking" style={{backgroundColor: '#2e9427'}}>Tracking</button>
+                            </div>
+                        </div>
+                        {/* Second Element - 25% */}
+                        <div  style={{ height: '25%'}}>
+                                
+                            </div>
+                        {/* Third Element - 25% */}
+                        <textarea className="bg-white text-black p-4 rounded-lg" style={{ height: '24%'}}>Note</textarea>
+                        {/* Fourth Element - 25% */}
+                        <div className="flex justify-end">
+                            <button className="bg-gray-500 text-white p-4 rounded-lg" id="stop">Stop</button>
+                            <button className="bg-red-500 text-white p-4 rounded-lg" id="pause">Pause</button>
+                            <button className="bg-green-500 text-white p-4 rounded-lg" id="start">Start</button>
+                        </div>
+                    </div>
+                </div>
 
-// This is the dashboard page. Make and Import components in /Components/x and setup the how the dashboard page works here. To play with layout use layout.tsx file
-export default async function Page() {
-
-	return (
-		<>
-			<h1 className="text-red-500 text-center align-middle">
-				THIS HERE IS MAIN PAGE
-			</h1>
-
-		</>
-	);
+            </div>
+        </>
+    );
 }
