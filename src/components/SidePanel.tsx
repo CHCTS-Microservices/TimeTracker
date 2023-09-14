ff850aad4555c51781ba0142e16a7422df48280b
Yitao Zhang <u7504766@anu.edu.au>
2023-09-14 17:47:50 +1000

    Sidie panel for release 1 needs to review Except dynamic time

--- src/components/SidePanel.tsx
+++ src/components/SidePanel.tsx
@@ -1,25 +1,22 @@
 import React, { useState } from 'react';
 import  * as Struct from '@/app/utils/types';
 
-
-
-// side panel decoration
-
-
+// Interface to define the expected props for the SidePanel component
 interface SidePanelProps {
     events: Struct.Event[];
   }
 
 
 function SidePanel( {events} : SidePanelProps) {
-    // const sortedEvents = [...events].sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
-    const sortedEvents = events.sort((a, b) => {
+    // sort events by active then by time
+    events.sort((a, b) => {
       if (a.active && !b.active) {
-          return -1; // active events come first
+          return -1; 
       } else if (!a.active && b.active) {
           return 1;
       } else {
-          return new Date(b.date).getTime() - new Date(a.date).getTime(); // sort by date if both have the same active status
+        // sort by date if both have the same active status
+          return new Date(b.date).getTime() - new Date(a.date).getTime();
       }
 
     
@@ -28,61 +25,76 @@ function SidePanel( {events} : SidePanelProps) {
 
   const EventCard: React.FC<{ event: Struct.Event }> = ({ event }) => {
     return (
-        <div className={`m-1 p-1 rounded-lg shadow-md`} style={{ background: event.active ? 'rgb(245, 206, 128)' : 'rgb(118, 167, 176)' }}>
-            <div className={`m-2 p-6 rounded-lg shadow-md text-white`} style={{ background: 'rgb(32, 63, 119)' }}>
-              <h2 className="text-xl font-semibold mb-2">{event.notes}</h2>
-              <p className="mb-2">Trail number: {event.trialID}</p>
-              <p className="mb-2">Stage number:</p>
-              {/* <p className="text-sm text-gray-500">{event.notes}</p> */}
+      // Container for the entire card, background color changes based on the event's active status
+        <div className={`m-1 p-1 rounded-lg shadow-md `} style={{ background: event.active ? 'rgb(245, 206, 128)' : 'rgb(118, 167, 176)' }}>
+          
+          {/* Using flexbox to layout inner elements of the card horizontally */}
+          <div className="flex space-x-2">
+
+            {/*Card: active label. Label indicating whether the event is active ("Tracking") or inactive */}
+            <div className={`w-[80px] h-[30px] mt-2 ml-2 rounded-lg shadow-lg text-white text-center  ${event.active ? 'bg-green-600' : 'bg-red-400'}`} >
+              <p className="text-xs font-semibold mb-2 mt-2 ml-0 ">{event.active ? 'Tracking' : 'Inactive'}</p>
             </div>
+
+            {/*Card: Side panel detail. Container for detailed information about the event */}
+            <div className={`w-[200px] h-[100px] rounded-lg shadow-lg text-white `} style={{ background: 'rgb(32, 63, 119)' }}>
+                <p className="text-xs m-2 font-semibold ">{event.notes}</p>
+                <p className="text-xs m-2">Trail number: {event.trialID}</p>
+                <p className="text-xs m-2">Stage:</p>
+            </div>
+          </div>
+
         </div>
     );
-}
+  }
 
 
 
-  
 
+    // State for managing the active filter status. 
+    // It can be either `true` (Trackinig), `false` (Inactive), or `null` (show all).
+    const [filterActive, setFilterActive] = useState<boolean | null>(null);
+
+    // Filtering the list of events based on the active status filter.
+    // If `filterActive` is null, show all events. Otherwise, show events that match the active status.
+    const filteredEvents = filterActive !== null
+        ? events.filter(event => event.active === filterActive)
+        : events;
 
   return (
-    <div className="w-64 h-full bg-gray-100 p-4 shadow-md " style={{ background: 'rgb(26, 97, 120)' }}>
-        {sortedEvents.map(event => (
-            <EventCard key={event.id} event={event} />
-        ))}
+
+        <div>
+          {/* Filter */}
+            <div className="m-1 p-1 ml-0 rounded-lg shadow-md w-64 h-full" style={{ background: 'rgb(245, 206, 128)'}}>
+                {/* Label indicating the purpose of the dropdown */}
+                <label>Select active status:</label>
+
+                {/* Dropdown to select the active status filter. 
+                 It updates the `filterActive` state when its value changes. */}
+                <select
+                    className="ml-2 p-1 border rounded"
+                    value={filterActive === null ? '' : filterActive.toString()}
+                    onChange={e => setFilterActive(e.target.value === '' ? null : e.target.value === 'true')}
+                >
+                    <option value="">All</option>
+                    <option value="true">Tracking</option>
+                    <option value="false">Inactive</option>
+                </select>
+            </div>
+
+            {/* Event Cards */}
+            <div className="w-64 h-full bg-gray-100 p-4 shadow-md " style={{ background: 'rgb(26, 97, 120)' }}>
+              {filteredEvents.map(event => (
+                <EventCard key={event.id} event={event} />
+              ))}
 
       
-    </div>
-  
-   
-    // <div className="w-64 h-full bg-gray-100 p-4 shadow-md " style={{ background: 'rgb(26, 97, 120)' }}>
-    //   <h3 className='text-xl mb-2.5'>Events</h3>
-    //   <ul className='pl-0 text-center'>
-    //     {sortedEvents.map(event => (
-    //       <div className="max-w-sm m-2 rounded overflow-hidden shadow-lg" style={{ background: 'rgb(32, 63, 119)' }}>
-    //         <div className="px-6 py-4 text-white font-bold mb-2 text-left">
-    //           Trail name: <span>{event.trialID}</span> <br />
-    //           Stage number: <span></span> <br />
-    //           ActicityID: <span>{event.activityID}</span> <br />
-
-    //         </div>
-    //       </div>
-    //       // <li key={event.id} className='py-1.5 border-b border-gray-300'>
-    //       //   Trail name: <span>{event.trialID}</span> <br />
-    //       //   Stage number: <span></span> <br />
-    //       //   ActicityID: <span>{event.activityID}</span> <br />
-    //       //   {/* <strong>{event.title}</strong><br /> */}
-            
-    //       //   {/* Date: <span>{event.date.toString()}</span><br /> */}
-    //       //   Active: <span>{event.active.toString()}</span>
-    //       // </li>
-    //     ))}
-    //   </ul>
-      
-    // </div>
+            </div>
+        </div>)
 
 
     
-  );
+  // );
 }
 
 export default SidePanel;
