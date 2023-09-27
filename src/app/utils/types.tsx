c1b35f11c0423091f031be9cf6a74a5b8edcf9a0
Artic13 <akshayj13@hotmail.com>
2023-09-27 23:30:14 +1000

    added in log functionality. Saves log into logs table when create, update or delete is done

--- src/app/utils/types.tsx
+++ src/app/utils/types.tsx
@@ -53,6 +53,12 @@ export type Duration = {
     minutes : number;
     seconds : number;
 }
+export type Log = {
+    staffID : number,
+    action : string,
+    eventID : number,
+    date : Date
+}
 
 // funtion that calculates duration
 
