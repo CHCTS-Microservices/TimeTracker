f05f114f29064fceccc2b6e7d932afad8fe5c541
Ran Xu <u7544719@anu.edu.au>
2023-10-02 20:14:38 +1100

    little modification

--- src/components/EventPopUp.tsx
+++ src/components/EventPopUp.tsx
@@ -1,117 +1,41 @@
 import React, { useState } from 'react';
 
 const EventPopup = () => {
-  // State to manage the visibility of the Trial dropdown
   const [showTrialDropdown, setShowTrialDropdown] = useState(false);
-  
-  // State to manage the visibility of the Activities dropdown
   const [showActivitiesDropdown, setShowActivitiesDropdown] = useState(false);
 
   return (
-      <div
-      className='rounded' 
-      //height:'819px'
-      style={{backgroundColor: 'rgb(26,97,120)', width: '1050px', right:'50px', top:'110px', height:'590px' , position: 'fixed'}} >
-        {/* Line 1 */}
-        <div style={{ margin: '20px' }}>
-          <label
-          className='rounded'
-          style={{
-            backgroundColor: 'rgb(37, 73, 133)', 
-            color: 'white', 
-            width: '80px',
-            height: '30px',
-            display: 'inline-block',
-            lineHeight: '30px', // To vertically center the text
-            textAlign: 'center' // To horizontally center the text
-        }} >Trial</label>
-          <select 
-          style={{ margin: '20px' }}
-          onClick={() => setShowTrialDropdown(!showTrialDropdown)}>
-            {/* Dropdown options */}
-            <option value="trial1">Trial 1</option>
-            <option value="trial2">Trial 2</option>
-            <option value="trial3">Trial 3</option>
-          </select>
-          <label 
-          className='rounded'
-          style={{
-            marginLeft:'50px',
-            backgroundColor: 'rgb(37, 73, 133)', 
-            color: 'white', 
-            width: '80px',
-            height: '30px',
-            display: 'inline-block',
-            lineHeight: '30px', // To vertically center the text
-            textAlign: 'center' // To horizontally center the text
-        }} >Stage2</label>
-        </div>
-  
-        {/* Line 2 */}
-        <div style={{ margin: '20px' }}>
-          <label 
-          className='rounded'
-          style={{
-            backgroundColor: 'rgb(37, 73, 133)', 
-            color: 'white', 
-            width: '80px',
-            height: '30px',
-            display: 'inline-block',
-            lineHeight: '30px', // To vertically center the text
-            textAlign: 'center' // To horizontally center the text
-        }} >Activity</label>
-          <select 
-          style={{ margin: '20px' }}
-          onClick={() => setShowActivitiesDropdown(!showActivitiesDropdown)}>
-            {/* Dropdown options */}
-            <option value="activity1">Activity 1</option>
-            <option value="activity2">Activity 2</option>
-            <option value="activity3">Activity 3</option>
-          </select>
-        </div>
-  
-        {/* Text Boxes */}
-        <div style={{ margin: '20px',marginTop:'60px' }}>
-          <label style={{color:'white'}} >Trial Details</label>
-          <label style={{ 
-            width: '100%',
-            height:'250px',
-            display: 'block', // or 'inline-block'
-            backgroundColor: 'white',
-            marginBottom: '10px',
-            boxSizing: 'border-box', // to include padding in element's height and width
-            }}></label>
-        </div>
-        {/* Confirm and Cancel Buttons */}
-      <div style={{ position: 'absolute', right: '20px', bottom: '20px' }}>
-        <button style={{
-          backgroundColor: '#4CAF50', /* Green */
-          color: 'white',
-          padding: '15px 32px',
-          textAlign: 'center',
-          textDecoration: 'none',
-          display: 'inline-block',
-          fontSize: '16px',
-          margin: '4px 2px',
-          cursor: 'pointer',
-          borderRadius: '4px'
-        }}>Confirm</button>
+    <div className="bg-[rgb(26,97,120)] w-[1050px] top-[110px] right-[50px] h-[590px] fixed rounded p-5">
+      <div className="mb-5">
+        <label className="bg-[rgb(37,73,133)] text-white w-[80px] h-[30px] inline-block rounded text-center">Trial</label>
+        <select className="mx-5" onClick={() => setShowTrialDropdown(!showTrialDropdown)}>
+          <option value="trial1">Trial 1</option>
+          <option value="trial2">Trial 2</option>
+          <option value="trial3">Trial 3</option>
+        </select>
+        <label className="bg-[rgb(37,73,133)] text-white w-[80px] h-[30px] inline-block rounded text-center ml-12">Stage2</label>
+      </div>
 
-        <button style={{
-          backgroundColor: '#f44336', /* Red */
-          color: 'white',
-          padding: '15px 32px',
-          textAlign: 'center',
-          textDecoration: 'none',
-          display: 'inline-block',
-          fontSize: '16px',
-          margin: '4px 2px',
-          cursor: 'pointer',
-          borderRadius: '4px'
-        }}>Cancel</button>
+      <div className="mb-5">
+        <label className="bg-[rgb(37,73,133)] text-white w-[80px] h-[30px] inline-block rounded text-center">Activity</label>
+        <select className="mx-5" onClick={() => setShowActivitiesDropdown(!showActivitiesDropdown)}>
+          <option value="activity1">Activity 1</option>
+          <option value="activity2">Activity 2</option>
+          <option value="activity3">Activity 3</option>
+        </select>
+      </div>
+
+      <div className="mb-5 mt-16">
+        <label className="text-white">Trial Details</label>
+        <label className="w-full h-[250px] block bg-white mb-2.5 box-border"></label>
+      </div>
+      
+      <div className="absolute bottom-5 right-5">
+        <button className="bg-green-500 text-white py-3 px-8 rounded text-center inline-block m-1 cursor-pointer">Confirm</button>
+        <button className="bg-red-500 text-white py-3 px-8 rounded text-center inline-block m-1 cursor-pointer">Cancel</button>
       </div>
     </div>
-    );
+  );
 };
 
 export default EventPopup;
