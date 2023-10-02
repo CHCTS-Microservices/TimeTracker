5935bb13d406b01acae5e46fce2c549bc17e604a
Ran Xu <u7544719@anu.edu.au>
2023-10-02 20:01:46 +1100

    A little modification

--- src/components/EventPopUp.tsx
+++ src/components/EventPopUp.tsx
@@ -10,13 +10,21 @@ const EventPopup = () => {
   return (
       <div
       className='rounded' 
-      
-      style={{backgroundColor: 'rgb(26,97,120)', width: '1050px', right:'50px', top:'110px', height: '819px', position: 'fixed'}} >
+      //height:'819px'
+      style={{backgroundColor: 'rgb(26,97,120)', width: '1050px', right:'50px', top:'110px', height:'590px' , position: 'fixed'}} >
         {/* Line 1 */}
         <div style={{ margin: '20px' }}>
-          <button
+          <label
           className='rounded'
-          style={{backgroundColor: 'rgb(37, 73, 133)', color:'white', width:'80px',height:'30px'}} >Trial</button>
+          style={{
+            backgroundColor: 'rgb(37, 73, 133)', 
+            color: 'white', 
+            width: '80px',
+            height: '30px',
+            display: 'inline-block',
+            lineHeight: '30px', // To vertically center the text
+            textAlign: 'center' // To horizontally center the text
+        }} >Trial</label>
           <select 
           style={{ margin: '20px' }}
           onClick={() => setShowTrialDropdown(!showTrialDropdown)}>
@@ -25,16 +33,33 @@ const EventPopup = () => {
             <option value="trial2">Trial 2</option>
             <option value="trial3">Trial 3</option>
           </select>
-          <button 
+          <label 
           className='rounded'
-          style={{ margin: '10px', backgroundColor: 'rgb(37, 73, 133)', color:'white',width:'80px',height:'30px'}}>Stage2</button>
+          style={{
+            marginLeft:'50px',
+            backgroundColor: 'rgb(37, 73, 133)', 
+            color: 'white', 
+            width: '80px',
+            height: '30px',
+            display: 'inline-block',
+            lineHeight: '30px', // To vertically center the text
+            textAlign: 'center' // To horizontally center the text
+        }} >Stage2</label>
         </div>
   
         {/* Line 2 */}
         <div style={{ margin: '20px' }}>
-          <button 
+          <label 
           className='rounded'
-          style={{backgroundColor: 'rgb(37, 73, 133)', color:'white', width:'80px',height:'30px'}}>Activity</button>
+          style={{
+            backgroundColor: 'rgb(37, 73, 133)', 
+            color: 'white', 
+            width: '80px',
+            height: '30px',
+            display: 'inline-block',
+            lineHeight: '30px', // To vertically center the text
+            textAlign: 'center' // To horizontally center the text
+        }} >Activity</label>
           <select 
           style={{ margin: '20px' }}
           onClick={() => setShowActivitiesDropdown(!showActivitiesDropdown)}>
@@ -46,13 +71,46 @@ const EventPopup = () => {
         </div>
   
         {/* Text Boxes */}
-        <div style={{ margin: '10px' }}>
+        <div style={{ margin: '20px',marginTop:'60px' }}>
           <label style={{color:'white'}} >Trial Details</label>
-          <textarea style={{ width: '100%',marginBottom:'10px',height:'150px'}}></textarea>
-          <label style={{color:'white'}}>Note</label>
-          <textarea style={{ width: '100%', height:'150px' }}></textarea>
+          <label style={{ 
+            width: '100%',
+            height:'250px',
+            display: 'block', // or 'inline-block'
+            backgroundColor: 'white',
+            marginBottom: '10px',
+            boxSizing: 'border-box', // to include padding in element's height and width
+            }}></label>
         </div>
+        {/* Confirm and Cancel Buttons */}
+      <div style={{ position: 'absolute', right: '20px', bottom: '20px' }}>
+        <button style={{
+          backgroundColor: '#4CAF50', /* Green */
+          color: 'white',
+          padding: '15px 32px',
+          textAlign: 'center',
+          textDecoration: 'none',
+          display: 'inline-block',
+          fontSize: '16px',
+          margin: '4px 2px',
+          cursor: 'pointer',
+          borderRadius: '4px'
+        }}>Confirm</button>
+
+        <button style={{
+          backgroundColor: '#f44336', /* Red */
+          color: 'white',
+          padding: '15px 32px',
+          textAlign: 'center',
+          textDecoration: 'none',
+          display: 'inline-block',
+          fontSize: '16px',
+          margin: '4px 2px',
+          cursor: 'pointer',
+          borderRadius: '4px'
+        }}>Cancel</button>
       </div>
+    </div>
     );
 };
 
