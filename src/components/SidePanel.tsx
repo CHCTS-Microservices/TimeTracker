1451a523a45b97fff885080a9992bd9951fd8812
zylan <zuhan.jian@anu.edu.au>
2023-10-04 15:15:28 +1100

    animation

--- src/components/SidePanel.tsx
+++ src/components/SidePanel.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import {Event, Duration} from '@/app/utils/types';
 import { timeCalc } from '@/app/utils/globalFuntions';
+import '@/app/globals.css'
 
 
 
@@ -95,7 +96,16 @@ function SidePanel({ events, selectedEvent, onEventSelect}: SidePanelProps) {
                 <p className="text-xs font-semibold">{event.active ? 'Tracking' : 'Inactive'}</p>
               </div>
 
-              
+              {/*Card: dynamic time. */}
+              <div>
+                {/* Loading spinner only shows when event is active */}
+                {event.active && 
+                  <div className="flex items-center justify-center w-[80px] h-[30px] mt-2 ml-2 rounded-lg">
+                    <div className="loading-spinner"></div>
+                  </div>
+                }
+              </div>
+
             </div>
 
             {/*Card: Side panel detail. Container for detailed information about the event */}
