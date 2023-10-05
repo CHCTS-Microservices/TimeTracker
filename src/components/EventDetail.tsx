d0af40fa78a48216fb8526d21b6d447ebacbfbfa
hisnameisjoseph <57517711+hisnameisjoseph@users.noreply.github.com>
2023-10-05 07:59:47 +0000

    added some comments for event details and metadata tsx

--- src/components/EventDetail.tsx
+++ src/components/EventDetail.tsx
@@ -4,18 +4,18 @@ import {Event, Trial, Activity} from "@/app/utils/types";
 type EventDetailProps = {
     event: Event,
 }
-
+// showing event details on the main page once user choose a event from side-panel
 export default function EventDetail({ event}: EventDetailProps) {
     return (
         <div className="inline-flex flex-col items-start gap-3 relative">
             <div className="pb-1/3">  {/* Maintain the aspect ratio */}
                 <div className="bg-[#254985] rounded-[15px] h-[200px] w-[490px] relative "> 
                     <div className="relative top-[12px] left-[12px] w-[478px] text-white font-extrabold text-[24px] leading-normal tracking-normal font-inter">
-                        {`Trial: ${event.trialName}`}
+                        {`Trial: ${event.trialName}`} {/*display trial name*/}
                         <br />
-                        {`Stage: ${event.stage}`}
+                        {`Stage: ${event.stage}`} {/*display current stage of trial*/}
                         <br />
-                        {`Activity: ${event.activityName}`}
+                        {`Activity: ${event.activityName}`} {/*display current activity*/}
                     </div>
                 </div>
             </div>
@@ -23,29 +23,4 @@ export default function EventDetail({ event}: EventDetailProps) {
     );
 };
 
-
-// import React from "react";
-// import { Event, Trial, Activity } from "@/app/utils/types";
-
-// type EventDetailProps = {
-//     event: Event,
-// }
-
-// export default function EventDetail({ event }: EventDetailProps) {
-//     return (
-//         <div className="relative w-1/3">
-//             <div className="pb-1/3"></div>  {/* Maintain the aspect ratio */}
-//                 <div className="absolute inset-0 bg-[#254985] rounded-[3vw]">
-//                     <div className="p-[2vw] text-white font-bold text-[2vw] font-inter">
-//                         {`Trial: ${event.trialName}`}
-//                         <br />
-//                         {`Stage: ${event.stage}`}
-//                         <br />
-//                         {`Activity: ${event.activityName}`}
-//                     </div>
-//                 </div>
-//             </div>
-
-//     );
-// }
 
--- src/components/Metadata.tsx
+++ src/components/Metadata.tsx
@@ -7,22 +7,21 @@ type MetadataProps = {
     event: Event,
 }
 
-
+// showing metadata details (relavent details from trial and activity) on the main page once user choose a event from side-panel
 export default function Metadata ({ event}: MetadataProps) {
     return (
         <div className="inline-flex flex-col items-start gap-3 h-72 relative">
             <div className="pb-1/3">  {/* Maintain the aspect ratio */}
                 <div className="bg-white rounded-[15px] h-[150px] w-[1000px] relative"> 
                     <div className="relative top-[12px] left-[12px] w-[462px] text-black font-extrabold text-[18px] leading-normal tracking-normal font-inter">
-                        {`Trial id: ${event.trialID}`}
+                        {`Trial id: ${event.trialID}`} {/*display trial id*/}
                         <br />
-                        {`Activity id: ${event.activityID}`}
+                        {`Activity id: ${event.activityID}`} {/*display activity id*/}
                         <br />
-                        {`Event id: ${event.id}`}
+                        {`Event id: ${event.id}`} {/*display event id*/}
                         <br />
-                        {`Unit: ${event.unit}`}
-                        {/* <br />
-                        {`Activity: ${event.activityName}`} */}
+                        {`Unit: ${event.unit}`} {/*display the associated clinical trial unit name*/}
+                        {/*add more detials if users recommend*/}
                     </div>
                 </div>
             </div>
