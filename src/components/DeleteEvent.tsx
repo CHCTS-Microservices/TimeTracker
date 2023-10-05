f62e9f34fdd40face40da02c6c9608ba9e44cdb7
Artic13 <akshayj13@hotmail.com>
2023-10-06 09:56:55 +1100

    fixed delete button popup

--- src/components/DeleteEvent.tsx
+++ src/components/DeleteEvent.tsx
@@ -36,35 +36,29 @@ export default function DeleteEvent( {deleteEvent} : DeleteEventProps) {
         </button>
       </div>
       <div>
-        <Dialog
-          open={open}
-          handler={handleOpen}
-          animate={{
-            mount: { scale: 1, y: 0 },
-            unmount: { scale: 0.9, y: -100 },
-          }}
-          //   className="flex h-screen justify-center items-center w"
-          // //   className="fixed inset-0 items-center justify-center h-60 w-80"
-          className="fixed inset-0 items-center justify-center h-60 w-80 "
-        >
-          <DialogHeader className="text-black">Delete Event</DialogHeader>
-          <DialogBody className="text-black" divider>
-            Are you sure you want to delete this event?
-          </DialogBody>
-          <DialogFooter>
-            <Button onClick={handleOpen} className="mr-1 bg-slate-600 p-4">
-              <span>Cancel</span>
-            </Button>
-            <Button
-              variant="gradient"
-              className="bg-red-700 p-4"
-              onClick={handelDelete}
-            >
-              <span>Confirm</span>
-            </Button>
-          </DialogFooter>
-        </Dialog>
+      <Dialog open={open} handler={handleOpen} size={"xs"}>
+        <DialogHeader>Delete Event</DialogHeader>
+        <DialogBody >
+          Are you sure you want to delete this event?
+        </DialogBody>
+        <DialogFooter>
+          <Button
+            variant="text"
+            color="blue"
+            onClick={handleOpen}
+            className="mr-1"
+          >
+            <span>Cancel</span>
+          </Button>
+          <Button variant="gradient" color="red" onClick={handelDelete}>
+            <span>Delete</span>
+          </Button>
+        </DialogFooter>
+      </Dialog>
+        
       </div>
     </div>
   );
 }
+
+
