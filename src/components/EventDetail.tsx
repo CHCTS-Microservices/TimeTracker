a8aa4c92f86bfef1f88e586b7484b16e4e1f0f28
Artic13 <126046296+Artic13@users.noreply.github.com>
2023-10-12 17:21:02 +1100

    Update EventDetail.tsx
    fixed

--- src/components/EventDetail.tsx
+++ src/components/EventDetail.tsx
@@ -10,7 +10,7 @@ export default function EventDetail({ event}: EventDetailProps) {
         <div className="items-start gap-3 relative">
             <div className="pb-1/3">  {/* Maintain the aspect ratio */}
                 <div className="ml-3 text-3xl font-bold text-black">
-                    Trail Information:
+                    Trial Information:
                 </div>
                 <div className="bg-[#254985] rounded-[15px] h-[160px] w-[380px] ml-0 flex items-center"> 
                     <div className="relative left-[12px] w-[478px] text-white font-extrabold text-2xl leading-normal tracking-normal font-inter truncate">
